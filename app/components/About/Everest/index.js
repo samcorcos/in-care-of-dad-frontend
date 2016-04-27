b18cd2a392f2ac8b1a889af539bf1f5aeef203e2
Sam Corcos <sam@corcos.io>
2016-04-27 15:34:01 -0400

    adding website select

--- app/components/About/Everest/index.js
+++ app/components/About/Everest/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import style from './style'
 
+import website from 'images/website-select.jpg'
+
 export const Everest = () => {
   return (
     <section styleName="everest">
@@ -9,7 +11,7 @@ export const Everest = () => {
 
       <div styleName="content">
         <div styleName="image">
-          <div styleName="placeholder"></div>
+          <img src={website} styleName="website" />
         </div>
         <div styleName="text">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

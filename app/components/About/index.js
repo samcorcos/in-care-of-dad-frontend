b6958c71007519c3c01f2d8318f6a07e28827b5c
Sam Corcos <sam@corcos.io>
2016-04-27 14:47:53 -0400

    refactoring into presentational component

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -3,6 +3,7 @@ import CSSModules from 'react-css-modules'
 import style from './style'
 
 import Video from 'components/About/Video'
+import Content from 'components/About/Content'
 import Footer from 'components/Footer'
 
 export class About extends React.Component {
@@ -14,8 +15,11 @@ export class About extends React.Component {
     return (
       <div>
         <section styleName="about">
+          <div styleName="overlay"></div>
           <Video />
+          <Content />
         </section>
+
         <Footer />
       </div>
     )

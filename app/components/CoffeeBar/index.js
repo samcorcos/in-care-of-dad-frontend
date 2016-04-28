3a4b8afbcf9b2292c8a9ebcc448c89176c3f8b10
Sam Corcos <sam@corcos.io>
2016-04-28 17:37:07 -0400

    coffee bar side panel

--- app/components/CoffeeBar/index.js
+++ app/components/CoffeeBar/index.js
@@ -2,16 +2,72 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import style from './style'
 
+const props = {
+  users: [
+    "samcorcos",
+    "kim",
+    "karen",
+    "john",
+    "sally"
+  ],
+  locations: [
+    "Seattle",
+    "San Francisco",
+    "Austin",
+    "New York",
+    "Houston",
+    "Dallas",
+    "San Antonio",
+    "Buffalo",
+    "Sacramento",
+    "Los Angeles",
+    "Bejing"
+  ]
+}
+
 export class CoffeeBar extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  randomNumber() {
+    return Math.ceil(Math.random() * 100)
+  }
+
   render() {
+    let self = this
+    const locations = props.locations.map((place) => {
+      return (
+        <div key={place} styleName="item">
+          <div styleName="location">{place}</div>
+          <div styleName="count">{self.randomNumber.bind(this)()}</div>
+        </div>
+      )
+    })
+    const people = props.users.map((person) => {
+      return (
+        <div key={person} styleName="item">
+          <div>{person}</div>
+        </div>
+      )
+    })
+
     return (
       <section styleName="coffee-bar">
         <div styleName="nav">
-          <h3 styleName="title">Coffee bar</h3>
+          <div styleName="location-container">
+            <h4 styleName="subtitle">Locations</h4>
+            <div styleName="list">
+              {locations}
+            </div>
+          </div>
+
+          <div styleName="message-container">
+            <h4 styleName="subtitle">Direct messages</h4>
+            <div styleName="list">
+              {people}
+            </div>
+          </div>
         </div>
       </section>
     )

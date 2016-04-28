b8fe97f3534b3f8285f02899b70aa901b903ca1d
Sam Corcos <sam@corcos.io>
2016-04-28 19:05:36 -0400

    posts

--- app/components/News/index.js
+++ app/components/News/index.js
@@ -2,12 +2,29 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import style from './style'
 
+const post = {
+    title: "This is the title of the post",
+    url: "https://source.unsplash.com/random",
+    body: "This is the body of the post, limited to a certain number of characters"
+  }
+
 export class News extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render() {
+
+    const item = (
+      <div styleName="post">
+        <img src={post.url} styleName="image" />
+        <div styleName="post-text">
+          <h4 styleName="post-title">{post.title}</h4>
+          <p styleName="post-body">{post.body}</p>
+        </div>
+      </div>
+    )
+
     return (
       <section styleName="news">
 
@@ -22,7 +39,11 @@ export class News extends React.Component {
           </div>
 
           <div>
-            body
+            {item}
+            {item}
+            {item}
+            {item}
+            {item}
           </div>
         </div>
 

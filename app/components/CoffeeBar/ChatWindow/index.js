8ecb051bfe1723c061c3ea4fc1786aeca23a181e
Sam Corcos <sam@corcos.io>
2016-05-02 10:30:28 -0400

    messages looking good

--- app/components/CoffeeBar/ChatWindow/index.js
+++ app/components/CoffeeBar/ChatWindow/index.js
@@ -4,14 +4,45 @@ import style from './style'
 
 import InputBar from 'components/CoffeeBar/InputBar'
 import Header from 'components/CoffeeBar/Header'
+import Message from 'components/CoffeeBar/Message'
 
-export const ChatWindow = props => {
-  return (
-    <section styleName="chat-window">
-      <Header />
-      <InputBar />
-    </section>
-  )
+export class ChatWindow extends React.Component {
+  constructor(props) {
+    super(props)
+    this.updateScroll = this.updateScroll.bind(this)
+  }
+
+  updateScroll() {
+    /* Sets the scroll to the bottom */
+    const element = document.getElementById("message-wrapper")
+    element.scrollTop = element.scrollHeight
+  }
+
+  componentDidMount() {
+    /* When the component mounts, set to the bottom */
+    /* TODO eventually we want to handle unread messages and change this */
+    this.updateScroll()
+  }
+
+  render() {
+    return (
+      <section styleName="chat-window">
+        <Header />
+
+        <div id="message-wrapper" styleName="message-wrapper">
+          {/* TODO obviously, this needs to map with data */}
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+        </div>
+
+        <InputBar />
+      </section>
+    )
+  }
 }
 
 export default CSSModules(ChatWindow, style)
--- app/components/CoffeeBar/Message/index.js
+++ app/components/CoffeeBar/Message/index.js
@@ -9,8 +9,15 @@ export class Message extends React.Component {
 
   render() {
     return (
-      <div>
-        Message component
+      <div styleName="message">
+        <div styleName="avatar"></div>
+        <div>
+          <span styleName="name">Bob Smith</span>
+          <span styleName="time-stamp">4:35pm</span>
+        </div>
+        <div styleName="message-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
       </div>
     )
   }
